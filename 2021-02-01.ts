9db8a8846594f4ba4f0b150394d551c9162db5fb
Evan Sutherland <stackoverfloweth@gmail.com>
2021-12-03 08:27:51 -0600

    eslinting

--- 2021-02-01.ts
+++ 2021-02-01.ts
@@ -10,33 +10,34 @@ type Instruction = {
   units: number
 
 }
+
 export function solve() {
-  // const input: Instruction[] = [
-  //   {direction:'forward', units:5},
-  //   {direction:'down', units:5},
-  //   {direction:'forward', units:8},
-  //   {direction:'up', units:3},
-  //   {direction:'down', units:8},
-  //   {direction:'forward', units:2},
-  // ]
+    // const input: Instruction[] = [
+    //     {direction:'forward', units:5},
+    //     {direction:'down', units:5},
+    //     {direction:'forward', units:8},
+    //     {direction:'up', units:3},
+    //     {direction:'down', units:8},
+    //     {direction:'forward', units:2},
+    // ]
 
-  const result = input.reduce((position: Position, instruction: Instruction) => {
-    switch (instruction.direction) {
-      case 'forward':
-        position.horizontal += instruction.units
-        break;
-      case 'up':
-        position.depth -= instruction.units
-        break;
-      case 'down':
-        position.depth += instruction.units
-        break;
-    }
+    const result = input.reduce((position: Position, instruction: Instruction) => {
+        switch (instruction.direction) {
+        case 'forward':
+            position.horizontal += instruction.units
+            break
+        case 'up':
+            position.depth -= instruction.units
+            break
+        case 'down':
+            position.depth += instruction.units
+            break
+        }
 
-    return position
-  }, { horizontal:0, depth:0 })
+        return position
+    }, { horizontal:0, depth:0 })
 
-  console.log(result.depth * result.horizontal)
+    console.log(result.depth * result.horizontal)
 }
 
 solve()
--- 2021-02-02.ts
+++ 2021-02-02.ts
@@ -12,33 +12,33 @@ type Instruction = {
 
 }
 export function solve() {
-  // const input: Instruction[] = [
-  //   {direction:'forward', units:5},
-  //   {direction:'down', units:5},
-  //   {direction:'forward', units:8},
-  //   {direction:'up', units:3},
-  //   {direction:'down', units:8},
-  //   {direction:'forward', units:2},
-  // ]
+    // const input: Instruction[] = [
+    //   {direction:'forward', units:5},
+    //   {direction:'down', units:5},
+    //   {direction:'forward', units:8},
+    //   {direction:'up', units:3},
+    //   {direction:'down', units:8},
+    //   {direction:'forward', units:2},
+    // ]
 
-  const result = input.reduce((position: Position, instruction: Instruction) => {
-    switch (instruction.direction) {
-      case 'forward':
-        position.horizontal += instruction.units
-        position.depth += position.aim * instruction.units
-        break;
-      case 'up':
-        position.aim -= instruction.units
-        break;
-      case 'down':
-        position.aim += instruction.units
-        break;
-    }
+    const result = input.reduce((position: Position, instruction: Instruction) => {
+        switch (instruction.direction) {
+        case 'forward':
+            position.horizontal += instruction.units
+            position.depth += position.aim * instruction.units
+            break
+        case 'up':
+            position.aim -= instruction.units
+            break
+        case 'down':
+            position.aim += instruction.units
+            break
+        }
 
-    return position
-  }, { horizontal:0, depth:0, aim:0 })
+        return position
+    }, { horizontal:0, depth:0, aim:0 })
 
-  console.log(result.depth * result.horizontal)
+    console.log(result.depth * result.horizontal)
 }
 
 solve()
