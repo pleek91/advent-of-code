681f79be546998fd1a93d6fbe6b740d38c6adb39
Craig Harshbarger <pleek91@gmail.com>
2022-12-02 14:55:50 -0600

    day 1 completed

--- days/01/part1.ts
+++ days/01/part1.ts
@@ -1,7 +1,19 @@
 import { sample, input } from './input'
 
 function solve(input: string) {
-  console.log(input)
+  const elves = input.split('\n\n').map(elf => elf.trim()).map(elf => elf.split('\n'))
+
+  const answer = elves.reduce((mostCalories, elf) => {
+    const calories = elf.reduce((sum, value) => parseInt(value) + sum, 0)
+
+    if (calories > mostCalories) {
+      return calories
+    }
+
+    return mostCalories
+  }, 0)
+
+  console.log(answer)
 }
 
-solve(sample)
+solve(input)
--- days/01/part2.ts
+++ days/01/part2.ts
@@ -1,7 +1,27 @@
 import { sample, input } from './input'
 
 function solve(input: string) {
-  console.log(input)
+  const elves = input.split('\n\n').map(elf => elf.trim()).map(elf => elf.split('\n'))
+
+  const [first, second, third] = elves.reduce(([first, second, third], elf) => {
+    const calories = elf.reduce((sum, value) => parseInt(value) + sum, 0)
+
+    if (calories > first) {
+      return [calories, first, second]
+    }
+
+    if (calories > second) {
+      return [first, calories, second]
+    }
+
+    if (calories > third) {
+      return [first, second, calories]
+    }
+
+    return [first, second, third]
+  }, [0, 0, 0])
+
+  console.log(first + second + third)
 }
 
-solve(sample)
+solve(input)
