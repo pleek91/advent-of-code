dd5a6048c38c3682370d7078461dbb1aae4d158c
Craig Harshbarger <pleek91@gmail.com>
2022-12-02 16:10:23 -0600

    Solve day 2

--- days/02/part1.ts
+++ days/02/part1.ts
@@ -1,7 +1,41 @@
 import { sample, input } from './input'
 
+// A = Rock
+// B = Paper
+// C = Scissors
+
+// X = Rock
+// Y = Paper
+// Z = Scissors
+
+// 1 = Rock
+// 2 = Paper
+// 3 = Scissors
+
 function solve(input: string) {
-  console.log(input)
+  const uniform = input.replaceAll('A', 'X').replaceAll('B', 'Y').replaceAll('C', 'Z')
+  const numbers = uniform.replaceAll('X', '1').replaceAll('Y', '2').replaceAll('Z', '3')
+  const matches = numbers.trim().split('\n').map(line => line.split(' ').map(char => parseInt(char)))
+
+  const answer = matches.reduce((points, [opponent, me]) => {
+    points += me
+
+    const difference = me - opponent
+
+    switch (difference) {
+      case 0:
+        points += 3
+        break
+      case 1:
+      case -2:
+        points += 6
+        break
+    }
+
+    return points
+  }, 0)
+
+  console.log(answer)
 }
 
-solve(sample)
+solve(input)
--- days/02/part2.ts
+++ days/02/part2.ts
@@ -1,7 +1,51 @@
 import { sample, input } from './input'
 
+// A = Rock
+// B = Paper
+// C = Scissors
+
+// X = lose
+// Y = draw
+// Z = win
+
+// 1 = Rock
+// 2 = Paper
+// 3 = Scissors
+
 function solve(input: string) {
-  console.log(input)
+  const uniform = input.replaceAll('A', '1').replaceAll('B', '2').replaceAll('C', '3')
+  const numbers = uniform.replaceAll('X', 'L').replaceAll('Y', 'D').replaceAll('Z', 'W')
+  const matches = numbers.trim().split('\n').map(line => line.split(' ')).map(([opponent, me]) => [parseInt(opponent), me] as const)
+
+  const answer = matches.reduce((points, [opponent, outcome]) => {
+    let choice = 0
+
+    if (outcome === 'L') {
+      choice = opponent - 1
+    }
+
+    if (outcome === 'D') {
+      choice = opponent
+      points += 3
+    }
+
+    if (outcome === 'W') {
+      choice = opponent + 1
+      points += 6
+    }
+
+    if (choice === 4) {
+      choice = 1
+    }
+
+    if (choice === 0) {
+      choice = 3
+    }
+
+    return points += choice
+  }, 0)
+
+  console.log(answer)
 }
 
-solve(sample)
+solve(input)
