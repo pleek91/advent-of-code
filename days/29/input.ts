c4bf5565745af2c3f55d500a80411b37e07a7d69
Craig Harshbarger <pleek91@gmail.com>
2022-12-02 16:13:37 -0600

    Delete extra days :doh:

--- days/29/input.ts
+++
@@ -1,7 +0,0 @@
-export const sample = `
-
-`
-
-export const input = `
-
-`
--- days/29/part1.ts
+++
@@ -1,7 +0,0 @@
-import { sample, input } from './input'
-
-function solve(input: string) {
-  console.log(input)
-}
-
-solve(sample)
