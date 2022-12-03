8cfc72da671ae8f9539142e96dfc253a3fa49bb5
Craig Harshbarger <pleek91@gmail.com>
2022-12-03 10:37:06 -0600

    Day 3

--- days/03/part1.ts
+++ days/03/part1.ts
@@ -1,7 +1,29 @@
 import { sample, input } from './input'
 
+const alphabet = 'abcdefghijklmnopqrstuvwxyz'
+
 function solve(input: string) {
-  console.log(input)
+  const packs = input.trim().split('\n').map(pack => [pack.slice(0, pack.length / 2), pack.slice(pack.length / 2)])
+  const items = packs.map(([a, b]) => {
+    for (const item of a) {
+      if (b.includes(item)) {
+        return item
+      }
+    }
+    throw 'No match found'
+  })
+
+  const answer = items.reduce((total, item) => {
+    const priority = alphabet.indexOf(item.toLocaleLowerCase()) + 1
+
+    if (item !== item.toLocaleLowerCase()) {
+      return total + priority + 26
+    }
+
+    return total + priority
+  }, 0)
+
+  console.log(answer)
 }
 
-solve(sample)
+solve(input)
--- days/03/part2.ts
+++ days/03/part2.ts
@@ -1,7 +1,35 @@
 import { sample, input } from './input'
 
+const alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
 function solve(input: string) {
-  console.log(input)
+  const elves = input.trim().split('\n')
+  const groups = elves.reduce<string[][]>((groups, elf) => {
+    const lastGroup = groups[groups.length - 1]
+
+    if (lastGroup && lastGroup.length < 3) {
+      lastGroup.push(elf)
+    } else {
+      groups.push([elf])
+    }
+
+    return groups
+  }, [])
+  const badges = groups.map(group => {
+    for (const char of alphabet) {
+      if (group.every(elf => elf.includes(char))) {
+        return char
+      }
+    }
+
+    throw 'No match found'
+  })
+
+  const answer = badges.reduce((total, badge) => {
+    return total + alphabet.indexOf(badge) + 1
+  }, 0)
+
+  console.log(answer)
 }
 
-solve(sample)
+solve(input)
