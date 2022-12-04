41bdc9f683d8e431c8cff2a839c862f773b69e3d
Craig Harshbarger <pleek91@gmail.com>
2022-12-04 13:49:40 -0600

    solve day 4

--- days/04/part2.ts
+++ days/04/part2.ts
@@ -1,7 +1,28 @@
 import { sample, input } from './input'
 
 function solve(input: string) {
-  console.log(input)
+  const pairs = input.trim()
+    .split('\n')
+    .map(line => line.split(','))
+    .map(([first, second]) => [first.split('-').map(Number), second.split('-').map(Number)])
+
+  const answer = pairs.reduce((total, [first, second]) => {
+    const [firstStart, firstEnd] = first
+    const [secondStart, secondEnd] = second
+
+    if (
+      firstStart >= secondStart && firstStart <= secondEnd
+      || firstEnd >= secondStart && firstEnd <= secondEnd
+      || secondStart >= firstStart && secondStart <= firstEnd
+      || secondEnd >= firstStart && secondEnd <= firstEnd
+    ) {
+      return total + 1
+    }
+
+    return total
+  }, 0)
+
+  console.log(answer)
 }
 
-solve(sample)
+solve(input)
