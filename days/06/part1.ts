370f91b660628e87437af3f92d600de052fa5411
Craig Harshbarger <pleek91@gmail.com>
2022-12-05 23:34:29 -0600

    solve day 6

--- days/06/part1.ts
+++ days/06/part1.ts
@@ -1,7 +1,23 @@
 import { sample, input } from './input'
 
-function solve(input: string) {
-  console.log(input)
+function solve(input: string, markerSize: number) {
+  let answer
+
+  for (let i = markerSize; i < input.length; i++) {
+    const chars = input.slice(i - markerSize, i)
+    const set = new Set(chars)
+
+    if (set.size === markerSize) {
+      answer = i
+      break
+    }
+  }
+
+  console.log(answer)
 }
 
-solve(sample)
+// part 1
+// solve(input, 4)
+
+// part 2
+solve(input, 14)
