8709d441d37a172c1455615a027117b46ff32c89
Craig Harshbarger <pleek91@gmail.com>
2022-12-07 23:41:41 -0600

    day 7

--- days/07/part1.ts
+++ days/07/part1.ts
@@ -1,7 +1,45 @@
 import { sample, input } from './input'
 
 function solve(input: string) {
-  console.log(input)
+  const commands = input.trim().split('\n')
+  const dirs: Record<symbol, number> = {}
+  const open: symbol[] = []
+
+  for (const command of commands) {
+    if (command.startsWith('dir') || command.startsWith('$ ls')) {
+      continue
+    }
+
+    if (command.startsWith('$ cd ..')) {
+      open.pop()
+      continue
+    }
+
+    if (command.startsWith('$ cd ')) {
+      open.push(Symbol())
+      continue
+    }
+
+    const [size] = command.split(' ')
+
+    open.forEach(dir => {
+      dirs[dir] ??= 0
+      dirs[dir] += parseInt(size)
+    })
+
+  }
+
+  const answer = Reflect.ownKeys(dirs).reduce((total, key) => {
+    const dirSize = dirs[key as symbol]
+
+    if (dirSize <= 100000) {
+      return total += dirSize
+    }
+
+    return total
+  }, 0)
+
+  console.log(answer)
 }
 
-solve(sample)
+solve(input)
--- days/07/part2.ts
+++ days/07/part2.ts
@@ -1,7 +1,44 @@
 import { sample, input } from './input'
 
 function solve(input: string) {
-  console.log(input)
+  const commands = input.trim().split('\n')
+  const dirs: Record<symbol, number> = {}
+  const open: symbol[] = []
+
+  for (const command of commands) {
+    if (command.startsWith('dir') || command.startsWith('$ ls')) {
+      continue
+    }
+
+    if (command.startsWith('$ cd ..')) {
+      open.pop()
+      continue
+    }
+
+    if (command.startsWith('$ cd ')) {
+      open.push(Symbol())
+      continue
+    }
+
+    const [size] = command.split(' ')
+
+    open.forEach(dir => {
+      dirs[dir] ??= 0
+      dirs[dir] += parseInt(size)
+    })
+
+  }
+
+  const sizes = Reflect.ownKeys(dirs).map(key => dirs[key as symbol])
+  const total = 70000000
+  const used = Math.max(...sizes)
+  const free = total - used
+  const update = 30000000
+  const needed = update - free
+  const filtered = sizes.filter(size => size >= needed)
+  const answer = Math.min(...filtered)
+
+  console.log(answer)
 }
 
-solve(sample)
+solve(input)
