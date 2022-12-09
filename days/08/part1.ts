34ed645755af24d3990d7cfa7e119b4ec43155a4
Craig Harshbarger <pleek91@gmail.com>
2022-12-08 22:52:02 -0600

    day 8

--- days/08/part1.ts
+++ days/08/part1.ts
@@ -1,7 +1,56 @@
 import { sample, input } from './input'
 
 function solve(input: string) {
-  console.log(input)
+  const rows = input.trim().split('\n')
+
+  let answer = 0
+
+  for (let rowIndex = 0; rowIndex < rows.length; rowIndex++) {
+    const previousRows = rows.slice(0, rowIndex)
+    const row = rows[rowIndex]
+    const nextRows = rows.slice(rowIndex + 1)
+
+    for (let columnIndex = 0; columnIndex < row.length; columnIndex++) {
+      const previousTrees = row.slice(0, columnIndex)
+      const tree = Number(row[columnIndex])
+      const nextTrees = row.slice(columnIndex + 1)
+
+      // console.log({ answer })
+      // console.log({ tree })
+
+      // left
+      const tallestToLeft = Math.max(...previousTrees.split('').map(Number))
+      if (tallestToLeft < tree) {
+        answer++
+        continue
+      }
+
+      // right
+      const tallestToRight = Math.max(...nextTrees.split('').map(Number))
+      if (tallestToRight < tree) {
+        answer++
+        continue
+      }
+
+      // top
+      const tallestToTop = Math.max(...previousRows.map(row => row[columnIndex]).map(Number))
+      if (tallestToTop < tree) {
+        answer++
+        continue
+      }
+
+      // bottom
+      const tallestToBottom = Math.max(...nextRows.map(row => row[columnIndex]).map(Number))
+      if (tallestToBottom < tree) {
+        answer++
+        continue
+      }
+
+    }
+
+  }
+
+  console.log(answer)
 }
 
-solve(sample)
+solve(input)
--- days/08/part2.ts
+++ days/08/part2.ts
@@ -1,7 +1,64 @@
 import { sample, input } from './input'
 
-function solve(input: string) {
-  console.log(input)
+function getDirectionScore(optionTree: number, trees: number[]): number {
+  let score = 0
+
+  for (const tree of trees) {
+
+    if (tree >= optionTree) {
+      score++
+      break
+    }
+
+    if (tree < optionTree) {
+      score++
+      continue
+    }
+
+    break
+  }
+
+  return score
 }
 
-solve(sample)
+function solve(input: string) {
+  const rows = input.trim().split('\n').map(row => row.split('').map(Number))
+  const options: number[] = []
+
+  for (let rowIndex = 0; rowIndex < rows.length; rowIndex++) {
+    const row = rows[rowIndex]
+
+    for (let columnIndex = 0; columnIndex < row.length; columnIndex++) {
+      const top = rows.slice(0, rowIndex).map(row => row[columnIndex])
+      const bottom = rows.slice(rowIndex + 1).map(row => row[columnIndex])
+      const left = row.slice(0, columnIndex)
+      const right = row.slice(columnIndex + 1)
+      const tree = row[columnIndex]
+
+      const visible = Math.max(...left) < tree ||
+                      Math.max(...right) < tree ||
+                      Math.max(...top) < tree ||
+                      Math.max(...bottom) < tree
+
+      if (!visible) {
+        continue
+      }
+
+      const topScore = getDirectionScore(tree, top.reverse())
+      const bottomScore = getDirectionScore(tree, bottom)
+      const leftScore = getDirectionScore(tree, left.reverse())
+      const rightScore = getDirectionScore(tree, right)
+      const treeScore = topScore * bottomScore * leftScore * rightScore
+
+      options.push(treeScore)
+
+    }
+
+  }
+
+  const answer = Math.max(...options)
+
+  console.log(answer)
+}
+
+solve(input)
