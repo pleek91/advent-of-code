42aeedaa7c05b180c6d7bc7eb7a5183a335d330f
Craig Harshbarger <pleek91@gmail.com>
2022-12-10 16:40:05 -0600

    day 10

--- days/10/part1.ts
+++ days/10/part1.ts
@@ -1,7 +1,42 @@
-import { sample, input } from './input'
+import { sample, sample2, input } from './input'
 
-function solve(input: string) {
-  console.log(input)
+function solveCycle(input: string, cycle: number) {
+  const commands = input.trim().split('\n')
+  let cycles = 0
+  let x = 1
+
+  for (const command of commands) {
+    cycles++
+
+    if (cycles == cycle) {
+      break
+    }
+
+    if (command === 'noop') {
+      continue
+    }
+
+    cycles++
+
+    if (cycles == cycle) {
+      break
+    }
+
+    const [, string] = command.split(' ')
+    const number = parseInt(string)
+
+    x += number
+  }
+
+  return cycle * x
 }
 
-solve(sample)
+function solve(input: string) {
+  const cycles = [20, 60, 100, 140, 180, 220]
+  const values = cycles.map(cycle => solveCycle(input, cycle))
+  const answer = values.reduce((total, current) => total + current, 0)
+
+  console.log(answer)
+}
+
+solve(input)
--- days/10/part2.ts
+++ days/10/part2.ts
@@ -1,7 +1,58 @@
-import { sample, input } from './input'
+import { sample, sample2, input } from './input'
 
 function solve(input: string) {
-  console.log(input)
+  const COMMANDS = input.trim().split('\n')
+  const CRT_ROW_LENGTH = 40
+  const CRT: string[] = []
+
+  let cycle = 0
+  let x = 1
+
+  function draw() {
+    let pixel = cycle
+    const sprite = [x - 1, x, x + 1]
+
+    while (pixel > 40) {
+      pixel -= 40
+    }
+
+    if (sprite.includes(pixel - 1)) {
+      CRT.push('#')
+    } else {
+      CRT.push('.')
+    }
+  }
+
+  for (const command of COMMANDS) {
+    cycle++
+    draw()
+
+    if (command === 'noop') {
+      continue
+    }
+
+    cycle++
+    draw()
+
+    const [, string] = command.split(' ')
+    const number = parseInt(string)
+
+    x += number
+  }
+
+  const rows = []
+
+  for (let i = 0; i < CRT.length; i += CRT_ROW_LENGTH) {
+    const row = CRT.slice(i, i + CRT_ROW_LENGTH)
+
+    if (row.length === CRT_ROW_LENGTH) {
+      rows.push(row)
+    }
+  }
+
+  const answer = rows.map(row => row.join('')).join('\n')
+
+  console.log(answer)
 }
 
-solve(sample)
+solve(input)
